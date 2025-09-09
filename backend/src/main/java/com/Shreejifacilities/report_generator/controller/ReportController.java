dca2c2fe406f0b297dc222ac1170db51ef2eacb3
Parth singh <ps.kuntal2005@gmail.com>
2025-09-10 01:49:52 +0530

    FIX: Set explicit context path for deployment

--- backend/src/main/java/com/Shreejifacilities/report_generator/controller/ReportController.java
+++ backend/src/main/java/com/Shreejifacilities/report_generator/controller/ReportController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/reports")
+@RequestMapping("/reports")
 public class ReportController {
 
 
